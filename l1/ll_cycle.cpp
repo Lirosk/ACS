94133dfc84e570db4979c3dca45a2f7d08849f0a
Losyash <kirill_crysis@bk.ru>
2021-03-01 00:58:37 +0300

    pointers

---
+++ l1/ll_cycle.cpp
@@ -0,0 +1,28 @@
+#include "ll_cycle.h"
+
+bool ll_has_cycle(node *head) {
+    /* Ваш код должен быть написан только внутри этой функции */
+
+    node* rabbit = head;
+    node* turtle = head;
+
+    for (;;)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (!rabbit->next)
+            {
+                return false;
+            }
+
+            rabbit = rabbit->next;
+        }
+
+        turtle = turtle->next;
+
+        if (turtle == rabbit)
+        {
+            return true;
+        }
+    }
+}
